9ccbae900e0258afea5a7c154bb59b2f41f08311
Néstor Javier Oubiña Arriaga <nestoroa@hotmail.com>
2018-11-09 06:43:33 +0100

    custom theming

---
+++ config/webpack.config.js
@@ -0,0 +1,21 @@
+{
+    test: /\.vue$/,
+    loader: 'vue-loader',
+    options: {
+        loaders: {
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+        }
+    }
+}
+{
+    test: /\.scss$/,
+    use: [
+      {
+        loader: 'css-loader'
+      },
+      {
+        loader: 'sass-loader'
+      }
+    ]
+  }
